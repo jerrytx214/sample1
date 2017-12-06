6b8b2edd1d7c3a189ca7c5e1ffc8ec996a8b40b5
Jose Rivera <jriverac@me.com>
2017-12-05 20:26:56 -0800

    About methods

--- some_file.rb
+++ some_file.rb
@@ -1,8 +1,11 @@
 puts "Quieres agregar nuevo?"
 puts "Si o no"
 puts "Nombre:Gerardo Rivera"
-print "Ano:2002" 
-puts def short_age(birth_year)
-Time.new.year - birth_year
+print "Ano:2002"
+
+# Methods cant be placed on a 'puts'
+def short_age(birth_year)
+  Time.new.year - birth_year
 end
+
 puts "Edad:#{short_age(2002)}"
